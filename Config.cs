9a1bd2d524e6b2940603d698357c92f20de1e5d5
Bdbmzwsc <88311279+Bdbmzwsc@users.noreply.github.com>
2021-11-25 13:31:32 +0800

    MIT

---
+++ Config.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAM
+{
+    /*
+     *用于保存 整个程序的一些默认配置变量
+     *无方法
+     */
+    class Config
+    {
+        //Github
+        public static string GithubUsername = "Bdbmzwsc";
+        public static string GithubToken = "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";
+        public static string User_Agent = "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.53";
+        //ContentType
+        public static string JSONcon = "application/json;";
+        public static string XMLcon = "application/xml;";
+        public static string TEXTcon = "tezt/plain;";
+        public static string HTMLcon = "text/html;";
+        //Email
+        public static string account_email = "227142580@qq.com";
+        public static string password_email = "klqnimzjyqjnbhed";
+    }
+}
---
+++ Stringbase64/base64.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAM.Stringbase64
+{
+    class base64
+    {
+        ///编码
+        public static string EncodeBase64(string code)
+        {
+               string encode = "";
+            //  byte[] bytes = Encoding.GetEncoding(code_type).GetBytes(code);
+            byte[] byteArray = System.Text.Encoding.Default.GetBytes(code);
+            try
+            {
+                encode = Convert.ToBase64String(byteArray);
+            }
+            catch
+            {
+                encode = code;
+            }
+            return encode;
+        }
+        ///解码
+        public static string DecodeBase64(string code)
+        {
+           // string decode = "";
+         //   byte[] bytes = Convert.FromBase64String(code);
+            
+                code = Encoding.Default.GetString(Convert.FromBase64String(code));
+                return code;
+
+            
+    
+        }
+    }
+}
